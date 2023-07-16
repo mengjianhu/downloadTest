42211b7a678461a6f20c1115373e3436183b7372
胡梦建 <2351491149@qq.com>
2023-07-16 15:01:03 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+      
+
+        
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FileDownloadController : ControllerBase
+    {
+        private readonly string _filePath = @"C:\Users\PC\Desktop\down2\test\t.pdf";
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadFile()
+        {
+            var memory = new MemoryStream();
+
+            using (var stream = new FileStream(_filePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, "application/octet-stream", Path.GetFileName(_filePath));
+        }
+    }
+}
